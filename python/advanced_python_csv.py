b9165818c83bed8286194b946dddb4fc5c41da8a
aliandra <aphelan10@gmail.com>
2016-12-16 22:02:38 -0500

    Answered advanced_python_csv

--- python/advanced_python_csv.py
+++ python/advanced_python_csv.py
@@ -1,1 +1,15 @@
-PLACE YOUR CODE HERE
+import csv
+import pandas as pd
+
+faculty = pd.read_csv('faculty.csv')
+faculty.columns = faculty.columns.str.strip()
+emails = faculty.email.tolist()
+print emails
+
+
+csvfile = "emails.csv"
+
+with open(csvfile, "w") as output:
+    writer = csv.writer(output, lineterminator='\n')
+    for email in emails:
+        writer.writerow([email]) 
